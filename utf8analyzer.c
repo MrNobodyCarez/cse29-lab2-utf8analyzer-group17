54c6e9b90f012c4f6bd5e1db57e9ead9176f64cb
Villanueva <c2villanueva@ieng6-201.ucsd.edu>
2025-07-09 12:32:18 -0700

    added capitilaize ascii

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -17,5 +17,15 @@ int main(int argc, char *argv[]) {
 		}
 		return 1;
 	}
+		int32_t capitalize_ascii(char str[]) {
+	int32_t count = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+        	if(str[i] >= 'a' && str[i] <= 'z'){
+                	str[i] = str[i] - ( 'a' - 'A');
+        	count++;
+        	}
+	}
+	return count;
+
     // implement the UTF-8 analyzer here
 }
